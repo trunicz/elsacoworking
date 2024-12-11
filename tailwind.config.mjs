ee5d61e4baed4d77dd2417edd9e1d8282f354fc2
Javier Oskar <tezjavi@gmail.com>
2024-12-10 22:49:09 -0700

    Nav y parte del hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,18 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          light: '#93C5FD', // Azul claro
+          DEFAULT: '#20618D', // Azul principal
+          dark: '#1E3A8A', // Azul oscuro
+        },
+        secondary: {
+          light: '#FDE68A', // Amarillo claro
+          DEFAULT: '#FBBF24', // Amarillo principal
+          dark: '#92400E', // Amarillo oscuro
+        },
+      },
     },
   },
   plugins: [],
