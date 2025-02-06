e40533150fe0ff650e4c5655b41d067d2a7cb1c9
trunicz <romelg.peralta@gmail.com>
2025-02-06 02:07:43 -0700

    feat: :sparkles: Add horizontal scroll to show benefits

--- src/landingPage/componentes/Benefits.jsx
+++ src/landingPage/componentes/Benefits.jsx
@@ -1,44 +1,56 @@
 import CardImage from "./Benefits/CardImage";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "motion/react";
 
 const images = [
   {
-    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg",
     alt: "Interior moderno de oficina",
   },
   {
-    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg",
     alt: "Interior moderno de oficina",
   },
   {
-    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg",
     alt: "Interior moderno de oficina",
   },
 ];
 
 const Benefits = () => {
-  return (
-    <section className="w-full mx-auto flex justify-center p-10">
-      <main className="text-center w-full">
-        <h1 className="text-4xl mx-auto font-titles font-black">Beneficios</h1>
-        <p className="font-euphemia w-8/12 mx-auto text-gray-600">
-          Aquí puedes proyectar una imagen profesional sin rentar una oficina
-          completa. Te ofrecen soporte administrativo, espacio para juntas y
-          atención para tus clientes. Ahorras tiempo y dinero, y te enfocas en
-          hacer crecer tu negocio sin preocuparte por los detalles.
-        </p>
+  const targetRef = useRef(null);
 
-        <div className="flex mx-auto py-6 w-2/3  h-96 overflow-hidden ">
-          {images.map((image, index) => (
-            <CardImage
-              key={index}
-              src={image.src}
-              alt={image.alt}
-              imageIndex={index}
-            />
-          ))}
-        </div>
-      </main>
-    </section>
+  const { scrollYProgress } = useScroll({ target: targetRef });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-53%"]);
+
+  return (
+    <div className="w-full mx-auto flex justify-center p-10">
+      <div className="text-center w-full">
+        <section
+          ref={targetRef}
+          className={`relative h-[${images.length}00vh]  mt-6`}
+        >
+          <div className="sticky top-0 h-screen w-8/12 mx-auto flex items-center flex-wrap">
+            <div className="flex-1 flex flex-col ">
+              <h1 className="text-4xl font-titles font-black">Beneficios</h1>
+              <p className="font-euphemia mx-auto text-gray-600">
+                Aquí puedes proyectar una imagen profesional sin rentar una
+                oficina completa. Te ofrecen soporte administrativo, espacio
+                para juntas y atención para tus clientes. Ahorras tiempo y
+                dinero, y te enfocas en hacer crecer tu negocio sin preocuparte
+                por los detalles.
+              </p>
+            </div>
+            <motion.div style={{ x }} className="flex items-center gap-6">
+              {images.map((image, index) => (
+                <CardImage src={image.src} alt={image.alt} key={index} />
+              ))}
+            </motion.div>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
 
--- src/landingPage/componentes/Benefits/CardImage.jsx
+++ src/landingPage/componentes/Benefits/CardImage.jsx
@@ -1,14 +1,18 @@
 import { motion } from "motion/react";
 
-export const CardImage = ({ src, alt, isActive, onClick, imageIndex }) => {
+export const CardImage = ({ src, alt }) => {
   return (
-    <motion.div
-      style={{ backgroundImage: `url(${src})` }}
-      drag="x"
-      dragConstraints={{ left: 0, right: 0 }}
-      animate={{ translateX: `${imageIndex * 100}%` }}
-      className="shrink-0 aspect-video object-cover w-screen"
-    />
+    <div className="w-[1080px] relative h-[70vh] overflow-hidden rounded-xl">
+      <div className="absolute bg-black/45 w-screen h-screen flex flex-col items-start p-24">
+        <h3 className="text-white font-titles text-6xl">Contenido</h3>
+        <p className="text-white/75">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+          laboriosam molestiae! Sed dignissimos illo dolorum ut obcaecati
+          excepturi voluptas est?
+        </p>
+      </div>
+      <img className="w-full h-full object-cover" src={src} alt={alt} />
+    </div>
   );
 };
 
