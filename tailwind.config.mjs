eb1f2bdb975c5658198518bb296b04bb4288deab
trunicz <romelg.peralta@gmail.com>
2025-02-11 18:23:16 -0700

    Feat: Implement Shadcn Components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,36 @@ export default {
           DEFAULT: '#FBBF24', // Amarillo principal
           dark: '#92400E', // Amarillo oscuro
         },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "#000000",
+        foreground: "#ffffff",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
         titles: ["Avenir, font-sans"],
