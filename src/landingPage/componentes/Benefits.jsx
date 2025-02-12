8cc353a249d5fb427937bfea6d4c75ab74740274
trunicz <romelg.peralta@gmail.com>
2025-02-11 18:05:28 -0700

    feat: :fire: UPDATE ALL SERVICE

--- src/landingPage/componentes/Benefits.jsx
+++ src/landingPage/componentes/Benefits.jsx
@@ -4,15 +4,15 @@ import { motion, useScroll, useTransform } from "motion/react";
 
 const images = [
   {
-    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg",
+    src: "https://picsum.photos/1080/720?grayscale",
     alt: "Interior moderno de oficina",
   },
   {
-    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg",
+    src: "https://picsum.photos/1080/720?grayscale",
     alt: "Interior moderno de oficina",
   },
   {
-    src: "https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg",
+    src: "https://picsum.photos/1080/720?grayscale",
     alt: "Interior moderno de oficina",
   },
 ];
@@ -22,13 +22,13 @@ const Benefits = () => {
 
   const { scrollYProgress } = useScroll({ target: targetRef });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-53%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-65%"]);
 
   return (
     <div className="w-full mx-auto flex justify-center p-10">
       <div className="text-center w-full">
-        <section ref={targetRef} className={`relative h-[200vh]  mt-6`}>
-          <div className="sticky top-0 h-screen w-8/12 mx-auto flex items-center flex-wrap">
+        <section ref={targetRef} className="relative h-[400vh]  mt-6">
+          <div className="sticky top-0 h-screen w-8/12 mx-auto flex items-center overflow-hidden flex-wrap">
             <div className="flex-1 flex flex-col ">
               <h1 className="text-4xl font-titles font-black">Beneficios</h1>
               <p className="font-euphemia mx-auto text-gray-600">
@@ -39,11 +39,22 @@ const Benefits = () => {
                 por los detalles.
               </p>
             </div>
-            <motion.div style={{ x }} className="flex items-center gap-6">
+            <motion.div
+              layout
+              style={{ x }}
+              className="flex items-center gap-6"
+            >
               {images.map((image, index) => (
                 <CardImage src={image.src} alt={image.alt} key={index} />
               ))}
             </motion.div>
+            <div className="flex-1 mx-auto">
+              <a
+                class={`inline-block px-6 py-3 rounded-full font-semibold primary bg-slate-600 text-white hover:bg-slate-700 cursor-pointer active:scale-95 transition-all select-none`}
+              >
+                Ver mas
+              </a>
+            </div>
           </div>
         </section>
       </div>
